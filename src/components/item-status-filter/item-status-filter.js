a06c8ab6098590a42972dff4bddc563eb17376fe
Dinzukhra Kalmakhan <kalmahan0420@icloud.com>
2024-11-08 23:10:16 +0500

    step 18

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,3 +1,18 @@
-import SearchPanel from './search-panel';
+import React, {Component} from 'react';
+import './item-staus-filter.css';
 
-export default SearchPanel;
+export default class ItemStatusFilter extends Component {
+
+    render () {
+        return (
+            <div className={"btn-group"}>
+                <button type={"button"}
+                        className={"btn btn-info"}>All list</button>
+                <button type={"button"}
+                        className={"btn btn-outline-secondary"}>Active list</button>
+                <button type={"button"}
+                        className={"btn btn-outline-secondary"}>Done</button>
+            </div>
+        );
+    }
+}
